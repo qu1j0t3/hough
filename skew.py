891fb76ddb85abcc5dcacbece594a01030cf7cd8
Toby Thain <toby.thain@fivebox.com>
2016-08-13 21:37:23 -0400

    Abandon the region-based detection. Make the rectangular dilation more aggressive to find more horizontal edges.

--- skew.py
+++ skew.py
@@ -56,13 +56,15 @@ for f in sys.argv[1:]:
         row = sums.argmax(0)
 
         # Detect edges
-        edges = binary_dilation( canny(pos[row-150:row+150], 2) )
+        cropped = pos[max(row-150, 0):min(row+150, pageh)]
+        edges = binary_dilation(canny(cropped, 1))
         edgesg = greyf(edges)
+
         # Now try Hough transform
         lines = probabilistic_hough_line(
                  edges,
                  line_length=pagew*0.15,
-                 line_gap=3,
+                 line_gap=2,
                  theta=arange(deg2rad(-93.0), deg2rad(-87.0), deg2rad(0.02)))
 
         angles = []
@@ -85,79 +87,49 @@ for f in sys.argv[1:]:
 
             t = threshold_otsu(neg)
 
-            # grab area +/- 100 pixels from row - this is about 3° rotation at 600dpi
-            thresh = neg[row-100:row+100] > t
-            labels = label(thresh, connectivity=1)
+            # If we didn't find a good feature at the horizontal sum peak,
+            # let's brutally dilate everything and look for a vertical margin
+            small = downscale_local_mean(neg, (2,2))
+            dilated = binary_dilation(small > t, rectangle(60, 60))
 
-            aspectmax = 0
-            thinnest = None
-            for r in regionprops(labels, pos[row-100:row+100]):
-                w = r.bbox[3]-r.bbox[1]
-                aspect = float(w) / (r.bbox[2]-r.bbox[0])
+            edges = canny(dilated, 3)
+            edgesg = greyf(edges)
+            # Now try Hough transform
+            th = np.concatenate( (arange(deg2rad(-93.0), deg2rad(-87.0), deg2rad(0.02)),
+                                  arange(deg2rad( -3.0), deg2rad(  3.0), deg2rad(0.02))) )
+            lines = probabilistic_hough_line(
+                     edges,
+                     line_length=pageh*0.04,
+                     line_gap=6,
+                     theta=th)
+            eprint(lines)
 
-                # If the aspect is < say 80? 60?, it's unlikely to be an actual rule,
-                # or it might be a rule intersected by text or other rules.
-                # Maybe in this case we could do Hough transform and try to pick
-                # very long lines?
-                if(aspect > aspectmax):
-                    aspectmax = aspect
-                    thinnest = r
-
-            if thinnest and aspectmax > 60:
-                a = rad2deg(thinnest.orientation)
-                # Drop other labels
-                def relabel(x):
-                    return x if x == thinnest.label else 0
-                relabel_func = np.vectorize(relabel)
-                relabelled = relabel_func(labels)
-                out = mark_boundaries(pos[row-100:row+100], relabelled, color=(1,0,0))
-                imsave('out/{}_rule.png'.format(filename), out)
-                eprint("{}  found rule: {}  aspect: {}  angle: {} deg".format(filename, thinnest.label, aspectmax, a))
-                angle = a
-            else:
-                # If we didn't find a good feature at the horizontal sum peak,
-                # let's brutally dilate everything and look for a vertical margin
-                small = downscale_local_mean(neg, (2,2))
-                dilated = binary_dilation(small > t, rectangle(60, 1))
-
-                edges = canny(dilated, 2)
-                edgesg = greyf(edges)
-                # Now try Hough transform
-                th = np.concatenate( (arange(deg2rad(-93.0), deg2rad(-87.0), deg2rad(0.02)),
-                                      arange(deg2rad( -3.0), deg2rad(  3.0), deg2rad(0.02))) )
-                lines = probabilistic_hough_line(
-                         edges,
-                         line_length=pageh*0.04,
-                         line_gap=6,
-                         theta=th)
-                eprint(lines)
-
-                angles = []
-                for ((x0,y0),(x1,y1)) in lines:
-                    if abs(x1-x0) > abs(y1-y0):
-                        # Horizontal - ensure it's moving East
-                        k = 1 if x1 > x0 else -1
-                        a = rad2deg(math.atan2(k*(y0-y1), k*(x1-x0)))
-                        eprint("{}    h line a: {}".format(filename, a))
-                    else:
-                        # Vertical - ensure it's moving South
-                        k = 1 if y1 > y0 else -1
-                        a = rad2deg(math.atan2(k*(x1-x0), k*(y1-y0)))
-                        eprint("{}    v line a: {}".format(filename, a))
-                    angles.append( a )
-                    rr, cc, val = line_aa(x0=x0, y0=y0, x1=x1, y1=y1)
-                    for k, v in enumerate(val):
-                        edgesg[rr[k], cc[k]] = (1-v)*edgesg[rr[k], cc[k]] + v
-
-                if angles:
-                    a = mean(angles)
-                    angle = a
-                    imsave('out/{}_{}_lines_vertical.png'.format(filename, a), edgesg)
-                    eprint("{}  angle vertical: {} deg (mean)  {} deg (median)".format(filename, a, median(angles)))
+            angles = []
+            for ((x0,y0),(x1,y1)) in lines:
+                if abs(x1-x0) > abs(y1-y0):
+                    # Horizontal - ensure it's moving East
+                    k = 1 if x1 > x0 else -1
+                    a = rad2deg(math.atan2(k*(y0-y1), k*(x1-x0)))
+                    eprint("{}    h line a: {}".format(filename, a))
                 else:
-                    imsave('out/{}_dilated.png'.format(filename), dilated)
-                    imsave('out/{}_dilate_edges.png'.format(filename), edges)
-                    eprint("{}  FAILED vertical".format(filename))
+                    # Vertical - ensure it's moving South
+                    k = 1 if y1 > y0 else -1
+                    a = rad2deg(math.atan2(k*(x1-x0), k*(y1-y0)))
+                    eprint("{}    v line a: {}".format(filename, a))
+                angles.append( a )
+                rr, cc, val = line_aa(x0=x0, y0=y0, x1=x1, y1=y1)
+                for k, v in enumerate(val):
+                    edgesg[rr[k], cc[k]] = (1-v)*edgesg[rr[k], cc[k]] + v
+
+            if angles:
+                a = mean(angles)
+                angle = a
+                imsave('out/{}_{}_lines_vertical.png'.format(filename, a), edgesg)
+                eprint("{}  angle vertical: {} deg (mean)  {} deg (median)".format(filename, a, median(angles)))
+            else:
+                imsave('out/{}_dilated.png'.format(filename), dilated)
+                imsave('out/{}_dilate_edges.png'.format(filename), edges)
+                eprint("{}  FAILED vertical".format(filename))
 
     if angle:
         print("'{}'".format(f))
